487c0bbbc8a87242c9b066f76d8f655e6a5ec429
lutchen <lutchen.henrique@gmail.com>
2022-02-10 20:07:27 -0300

    commitando ultima alteração javascript aula

---
+++ JavaScripts/script.js
@@ -0,0 +1,42 @@
+
+var myObject = {
+    color:"yellow",
+    length: 15,
+    write: function(){
+        console.log("Escreveu");
+    }
+
+}
+
+var myArray = [12,32,43,54,65,76]
+
+
+//transforma objeto em JSON
+//JSON.stringify(myObject)
+
+//var myString = JSON.stringify(myObject)
+
+//Transforma a string em JSON.
+//JSON.parse(myString)
+
+
+//objeto elevador com funções para subir e descer.
+var elevator = {
+    floor:0,
+    total:3,
+    up(){
+        if(this.floor < this.total){
+            this.floor++
+        }
+        this.print(this.floor)
+    },
+    down(){
+        if(this.floor > 0){
+            this.floor--
+        }
+        this.print(this.floor)
+    },
+    print(){
+        console.log( `Andar: ${elevator.floor}` )
+    } 
+}
